8ceaba6c050a614d65f8fd249d56b4b3c7060d7a
John Walsh <john.walsh16609@gmail.com>
2016-02-29 16:52:22 +0000

    Myo Control Code Added
    Myo control code has been added, but its not yet implemented fully to
    control the car.
    More updates soon...

--- src/myo_car_controller.ino
+++ src/myo_car_controller.ino
@@ -1,3 +1,7 @@
+// The myo libray
+#include <MyoController.h>
+
+// Defining motor control pins, ultrasonic sensor pins & motor speeds 
 #define motorLeftSpeed 5
 #define motorLeftOnOff 6
 #define motorRightOnOff 10
@@ -10,23 +14,56 @@
 #define motorFast 120
 #define motorBrake 0
 
+// Variables for ultrasonic sensor
 long duration, inches, cm;
 
+MyoController myo = MyoController();
+
+// Init setup function
 void setup(){
     Serial.begin(9600); // setup serial
     pinMode(motorLeftSpeed, OUTPUT);
     pinMode(motorLeftOnOff, OUTPUT);
     pinMode(motorRightSpeed, OUTPUT);
     pinMode(motorRightOnOff, OUTPUT);
+    myo.initMyo();
 }
 
+// Loop function
 void loop(){
+    myo.updatePose();
+    
+    switch ( myo.getCurrentPose() ) {
+    case rest:
+        // Reset
+        break;
+    case fist:
+        // Call forward function
+        break;
+    case waveIn:
+        // Turn left function call here
+        break;
+    case waveOut:
+        // Turn right function call here
+        break;
+    case fingersSpread:
+        // Call the break function
+        break;
+    }
+    
+    delay(100);
+    
+    // Sending pulses & receive pulses on ultrasonic sensor
     sendPulse();
     rcvPulse();
+    // Converting distance from ultrasonic sensor to cm
     convertToDistance();
     Serial.println(cm);
     delay(250);
-    
+
+    // If no object found within 100 cm then set motor speed to medium
+    // Else if object found within 75 to 100 cm then set motor speed to slow
+    // Else if object within 35 cm then break, reverse & turn left
     if (cm > 100){
         Forward(motorMedium);
     }
@@ -43,6 +80,7 @@ void loop(){
     }
 }
 
+// Function to propel motors forward
 void Forward(int motorSpeed){
     Serial.println("Forward");
     delay(250);
@@ -52,6 +90,7 @@ void Forward(int motorSpeed){
     digitalWrite(motorRightOnOff, LOW);
 }
 
+// Function to propel motors in reverse
 void Reverse(int motorSpeed){
     Serial.println("Reverse");
     delay(250);
@@ -61,6 +100,7 @@ void Reverse(int motorSpeed){
     analogWrite(motorRightOnOff, motorSpeed);
 }
 
+// Function to change the direction of the motors to turn left
 void TurnLeft(int motorSpeed){
     Serial.println("Turn Right");
     delay(250);
@@ -70,6 +110,7 @@ void TurnLeft(int motorSpeed){
     digitalWrite(motorRightOnOff, LOW);
 }
 
+// Function to change the direction of the motors to turn right
 void TurnRight(int motorSpeed){
     Serial.println("Turn Left");
     delay(250);
@@ -79,6 +120,7 @@ void TurnRight(int motorSpeed){
     analogWrite(motorLeftSpeed, motorSpeed);
 }
 
+// Function to stop motors
 void Brake(int motorSpeed){
     Serial.println("Brake");
     delay(125);
@@ -88,6 +130,7 @@ void Brake(int motorSpeed){
     digitalWrite(motorRightOnOff, LOW);
 }
 
+// Function to convert microseconds to inches
 long microsecondsToInches(long microseconds){
     // According to Parallax's datasheet for the PING))), there are
     // 73.746 microseconds per inch (i.e. sound travels at 1130 feet per
@@ -97,6 +140,7 @@ long microsecondsToInches(long microseconds){
     return microseconds / 74 / 2;
 }
 
+// Function to convert microseconds to cm
 long microsecondsToCentimeters(long microseconds){
     // The speed of sound is 340 m/s or 29 microseconds per centimeter.
     // The ping travels out and back, so to find the distance of the
@@ -104,6 +148,7 @@ long microsecondsToCentimeters(long microseconds){
     return microseconds / 29 / 2;
 }
 
+// Function to send ultrasonic pulses
 void sendPulse(){
     // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
     // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
@@ -115,6 +160,7 @@ void sendPulse(){
     digitalWrite(trigPin, LOW);
 }
 
+// Function to receive ultrasonic pulses
 void rcvPulse(){
     // Read the signal from the sensor: a HIGH pulse whose
     // duration is the time (in microseconds) from the sending
@@ -123,6 +169,7 @@ void rcvPulse(){
     duration = pulseIn(echoPin, HIGH);
 }
 
+// Function to convert time into a distance
 void convertToDistance(){
     // convert the time into a distance
     inches = microsecondsToInches(duration);
@@ -130,4 +177,3 @@ void convertToDistance(){
     Serial.println(cm);
     delay(50);
 }
-
